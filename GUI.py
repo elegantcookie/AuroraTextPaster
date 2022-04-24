8e9509e42858d64532597cbd3e8d214ad7e761d1
ribalko33 ribalko33 <gladiolud64@gmail.com>
2022-04-24 23:53:52 +0300

    pause added

--- GUI.py
+++ GUI.py
@@ -1,12 +1,13 @@
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
-import time
-from paster import Paster
+from PyQt5.QtCore import QThread
+from Worker import Worker
+
 
 class MainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
-        # Add a title
+
         self.setWindowTitle("AuroraTextPaster")
 
         # Set Vertical layout
@@ -21,52 +22,66 @@ class MainWindow(qtw.QWidget):
         self.layout().addWidget(my_label)
 
         # Create an Text box
-        my_text = qtw.QTextEdit(self,
-                                acceptRichText=False,
-                                lineWrapMode=qtw.QTextEdit.FixedColumnWidth,
-                                lineWrapColumnOrWidth=75,
-                                placeholderText="Вставьте сюда текст...",
-                                )
+        self.my_text = qtw.QTextEdit(self,
+                                     acceptRichText=False,
+                                     lineWrapMode=qtw.QTextEdit.FixedColumnWidth,
+                                     lineWrapColumnOrWidth=75,
+                                     placeholderText="Вставьте сюда текст...",
+                                     )
 
         # Change font size of spinbox
         # my_spin.setFont(qtg.QFont('Helvetica', 18))
 
         # Put combobox on the screen
-        self.layout().addWidget(my_text)
+        self.layout().addWidget(self.my_text)
 
         # Create a button
-        pbutton = qtw.QPushButton("Начать вставку",
-                                    clicked=lambda: textpaste())
-        self.layout().addWidget(pbutton)
+        self.pbutton = qtw.QPushButton("Начать вставку", self)
+        self.pbutton.clicked.connect(self.textpaste)
+        self.layout().addWidget(self.pbutton)
 
-        sbutton = qtw.QPushButton("Приостановить",
-                                  clicked=lambda: stop())
+        self.sbutton = qtw.QPushButton("Приостановить", self)
+        self.sbutton.clicked.connect(self.stop)
 
-        self.layout().addWidget(sbutton)
-        self.paster = Paster()
+        self.layout().addWidget(self.sbutton)
 
         # Show the app
         self.show()
 
-        def textpaste():
-            # Add name to label
-            time.sleep(3)
+    def textpaste(self):
+        # Step 2: Create a QThread object
+        self.thread = QThread()
+        # Step 3: Create a worker object
+        self.worker = Worker()
+        # Step 4: Move worker to the thread
+        self.worker.moveToThread(self.thread)
+        # Step 5: Connect signals and slots
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        # Step 6: Start the thread
+        self.thread.start()
+        self.pbutton.setEnabled(False)
+        self.thread.finished.connect(
+            lambda: self.pbutton.setEnabled(True)
+        )
 
-            with open('file.txt', 'w', encoding='utf8') as f:
-                stri = my_text.toPlainText()
-                f.write(stri)
+        with open('file.txt', 'w', encoding='utf8') as f:
+            stri = self.my_text.toPlainText()
+            f.write(stri)
+        # Add name to label
 
-            self.paster.read_from_file()
-            self.paster.strip_contents()
-            self.paster.start()
-
-        def stop():
-            self.paster.ongoing = not self.paster.ongoing
+    def stop(self):
+        self.worker.paster.ongoing = not self.worker.paster.ongoing
+        if self.sbutton.text() == 'Приостановить':
+            self.sbutton.setText('Возобновить')
+        else:
+            self.sbutton.setText('Приостановить')
 
 
 
 app = qtw.QApplication([])
 mw = MainWindow()
-
-# Run The App
 app.exec_()
+
