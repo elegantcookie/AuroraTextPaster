de3eef335a063798f81a88cf630c3cef9095b23e
ribalko33 ribalko33 <gladiolud64@gmail.com>
2022-04-24 21:08:28 +0300

    initial commit

---
+++ paster.py
@@ -0,0 +1,43 @@
+import time, os
+from pynput.keyboard import Key, Controller
+
+
+class Paster:
+    def __init__(self):
+        self.ongoing = True
+        self.keyboard = Controller()
+        self.contents = []
+
+    def read_from_file(self):
+        with open('file.txt', 'r', encoding='utf8') as f:
+            self.contents = f.readlines()
+        os.remove('file.txt')
+
+    def strip_contents(self):
+        for i in range(len(self.contents)):
+            self.contents[i] = self.contents[i].rstrip('\n')
+
+    def start(self):
+        for i in range(len(self.contents)):
+            for j in range(len(self.contents[i])):
+                if self.ongoing:
+                    if self.contents[i][j] == '.':
+                        self.keyboard.press('.')
+                        self.keyboard.release('.')
+                    else:
+                        self.keyboard.press(self.contents[i][j])
+                        self.keyboard.release(self.contents[i][j])
+                        time.sleep(0.095)
+                else:
+                    time.sleep(3)
+            self.keyboard.press(Key.enter)
+            self.keyboard.release(Key.enter)
+
+
+
+
+
+
+
+
+
