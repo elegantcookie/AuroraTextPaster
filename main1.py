62be693f2938d6fea90dbe5fe5ccd14eac977d61
ribalko33 ribalko33 <gladiolud64@gmail.com>
2022-04-24 22:11:26 +0300

    check

---
+++ main1.py
@@ -0,0 +1,41 @@
+import time
+from pynput.keyboard import Key, Controller
+
+
+
+keyboard = Controller()
+delay_time = 3  #s
+
+print(f'Программа вставляет код КУДА УГОДНО, поэтому у вас есть {delay_time} секунды, чтобы переключиться на нужное '
+      f'поле ввода')
+print("Введите код программы. Для завершения введите символ '~' с новой строки:")
+
+contents = []
+while True:
+    try:
+        line = input()
+        if line == '~':
+            break
+    except EOFError:
+        break
+    contents.append(line)
+
+for i in range(len(contents)):
+    contents[i] = contents[i].strip('\t')
+
+time.sleep(delay_time)
+
+for i in range(len(contents)):
+    for j in range(len(contents[i])):
+        print(contents[i][j])
+        if contents[i][j] == '.':
+            keyboard.press('.')
+            keyboard.release('.')
+        else:
+            keyboard.press(contents[i][j])
+            keyboard.release(contents[i][j])
+            time.sleep(0.095)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
